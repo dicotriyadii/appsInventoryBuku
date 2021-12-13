5acef36f580a8cab3017d49208bde4e54e938fb5
mdico.triyadi <mdico.triyadi@pt-ssss.com>
2021-12-13 15:59:19 +0700

    first commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
+$route['login']              = 'Clogin';
+$route['dashboard']          = 'CPage';
+
+// proses 
+$route['prosesLogin']  = 'Clogin/prosesLogin';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
