c4188c0afafcae9b4958c7e4aa27ca9ceca8ee36
mdico.triyadi <mdico.triyadi@pt-ssss.com>
2021-12-14 16:48:24 +0700

    Update

--- application/controllers/CSimpan.php
+++ application/controllers/CSimpan.php
@@ -6,6 +6,7 @@ class CSimpan extends CI_Controller {
     function __construct(){
         parent::__construct();
         $this->load->model('MData');
+        $this->load->library('pdf');
     }
 
 	function index()
@@ -47,5 +48,40 @@ class CSimpan extends CI_Controller {
         }else{
             echo "gagal";
         }
+    }
+
+    function cetakPdf (){
+        $pdf = new FPDF('l','mm','A5');
+        // membuat halaman baru
+        $pdf->AddPage();
+        // setting jenis font yang akan digunakan
+        $pdf->SetFont('Arial','B',16);
+        // mencetak string 
+        $pdf->Cell(190,7,'Aplikasi Inventaris Buku',0,1,'C');
+        $pdf->SetFont('Arial','B',12);
+        $pdf->Cell(190,7,'Daftar buku yang sudah menjadi inventaris perpustakaan',0,1,'C');
+        // Memberikan space kebawah agar tidak terlalu rapat
+        $pdf->Cell(10,7,'',0,1);
+        $pdf->SetFont('Arial','B',10);
+        $pdf->Cell(30,6,'Tanggal',1,0);
+        $pdf->Cell(50,6,'Asal Buku',1,0);
+        $pdf->Cell(40,6,'Nomor Inventaris',1,0);
+        $pdf->Cell(30,6,'Pengarang',1,0);
+        $pdf->Cell(20,6,'Judul',1,0);
+        $pdf->Cell(20,6,'Penerbit',1,1);
+
+        $pdf->SetFont('Arial','',10);
+        $inventarisBuku = $this->db->get('tbl_inventaris')->result();
+        foreach ($inventarisBuku as $row){
+            $pdf->Cell(30,6,$row->tanggal,1,0);
+            $pdf->Cell(50,6,$row->asalBuku,1,0);
+            $pdf->Cell(40,6,$row->nomorInventaris,1,0);
+            $pdf->Cell(30,6,$row->pengarang,1,0); 
+            $pdf->Cell(20,6,$row->judul,1,0); 
+            $pdf->Cell(20,6,$row->penerbit,1,1); 
+        }
+        
+        $pdf->Output();
+
     }
 }
